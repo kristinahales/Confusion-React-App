0f9739d315e134542479fc430bb1a91de549f913
Kristina Hales <krissyhales22@churchofjesuschrist.org>
2020-05-29 13:53:04 -0600

    incorporated props and designed menu items to be cards

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
+    let {dishes} = this.state;
     return (
       <div>
         <Navbar dark color='primary'>
@@ -12,7 +21,8 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={dishes}/>
+        {/* <Menu dishes = {this.state.dishes}/>  another option if not deconstructing dishes in render portion*/}
       </div>
     );
   }
